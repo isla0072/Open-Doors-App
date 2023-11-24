64019f249a80726ecd875a15811c7b272ec5a203
Abir Islah <isla0072@algonquinlive.com>
2023-11-24 14:06:35 -0500

    Initial Commit

---
+++ DoorsFinalApp/DoorsFinalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoorsFinalAppApp.swift
+//  DoorsFinalApp
+//
+//  Created by Winter Semester on 2023-11-24.
+//
+
+import SwiftUI
+
+@main
+struct DoorsFinalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
