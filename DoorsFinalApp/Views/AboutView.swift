292c815dff92103277150c3ed261cb4fad828f76
Abir Islah <isla0072@algonquinlive.com>
2023-12-15 23:33:37 -0500

    fixed launched and about page

--- DoorsFinalApp/Views/AboutView.swift
+++ DoorsFinalApp/Views/AboutView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AboutView: View {
     var name: String = "Abir Islah"
     var email: String = "isla0072@algonquinlive.com"
+    var github: String = "https://github.com/isla0072"
     var image: Image = Image("about")
 
     var body: some View {
@@ -21,8 +22,22 @@ struct AboutView: View {
             .clipShape(RoundedRectangle(cornerRadius: 10))
             Text(name)
                 .font(.title)
+            Divider()
+            HStack {
+            Image(systemName: "envelope")
+                .foregroundColor(.gray)
             Text(email)
                 .font(.subheadline)
+            }
+            Divider()
+            HStack {
+            Image(systemName: "link")
+                .foregroundColor(.gray)
+            Link(destination: URL(string: github)!, label: {
+            Text(github)
+                .font(.subheadline)
+                })
+            }
             Spacer()
         }
         .padding()
